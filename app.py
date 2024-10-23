95407da214af4a1b4f285e46ffa4e0fe1cd92833
Pablo Suarez <47283527+PabloSR06@users.noreply.github.com>
2024-10-23 22:03:13 +0100

    feat: accept post

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, jsonify, request
-from json.old.azureOpenAIClient import AzureOpenAIClient
+from azureOpenAIClient import AzureOpenAIClient
 
 from recognizer.saveInDatabase import json_to_database # type: ignore
 
@@ -7,12 +7,23 @@ from recognizer.saveInDatabase import json_to_database # type: ignore
 app = Flask(__name__)
 
 
-@app.route('/api/ask/<question>', methods=['GET'])
-def ask_for_data(question):
+@app.route('/api/ask/<question>', methods=['GET', 'POST'])
+@app.route('/api/ask', methods=['POST'])
+def ask_for_data(question=None):
+    if request.method == 'POST':
+        data = request.json
+        if data and 'question' in data:
+            question = data['question']
+    
+    if question is None:
+        return jsonify({'error': 'No question provided'}), 400
+
+
     azure_client = AzureOpenAIClient()
     response = azure_client.gerResponse(question)
+
     if response is None:
-        return jsonify({"error": "No se pudo obtener los puntos del arquero"}), 500
+        return jsonify({"error": "No se pudo obtener datos"}), 500
     return jsonify({"response": response})
 
 @app.route('/api/process/json', methods=['GET'])
@@ -21,6 +32,12 @@ def process_json():
     if response is None:
         return jsonify({"error": "No se pudo procesar"}), 500
     return jsonify({"response": response})
+
+@app.route('/')
+def index():
+    return jsonify({
+        "message": "Para hacer una pregunta, ve a la URL /api/ask/<question>"
+    })
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
